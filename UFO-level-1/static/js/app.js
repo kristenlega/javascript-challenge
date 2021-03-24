3b4e478e13255d6000307dc605a9c577e4041dd1
Kristen Lega <kristenlega@gmail.com>
2021-03-23 20:55:41 -0500

    Adding initial javascript code to add table rows.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+data.forEach(function(sightings) {
+  // console.log(sightings);
+  var row = tbody.append("tr");
+  Object.entries(sightings).forEach(function([key, value]) {
+   // console.log(key, value);
+
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
