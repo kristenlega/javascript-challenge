61b7b04a6826cb1035e956ede8b3d496565a2a17
Kristen Lega <kristenlega@gmail.com>
2021-03-24 20:07:16 -0500

    Adding initial code for table and filter.

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,9 +1,8 @@
 // from data.js
 var tableData = data;
 
-// assign variable to the table body element
+// assign varable to the table body element
 var tbody = d3.select("tbody");
-
 
 // for each element within the data
 data.forEach(function(sightings) {
@@ -13,7 +12,7 @@ data.forEach(function(sightings) {
   var row = tbody.append("tr");
   // for each item within the element
   Object.entries(sightings).forEach(function([key, value]) {
-  // console.log(key, value);
+   // console.log(key, value);
     // assign a variable to the new cells in the new row
     var cell = row.append("td");
     // input the value of the item into the cell
@@ -22,56 +21,32 @@ data.forEach(function(sightings) {
 });
 
 
-
 // Select the button
 var button = d3.select("#filter-btn");
- 
+​
 // Select the form
-var form = d3.select("#form");
-
+var form = d3.select(".form-group");
+​
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit", runEnter);
-
+​
 // Complete the event handler function for the form
 function runEnter() {
-
+​
   // Prevent the page from refreshing
   d3.event.preventDefault();
-
-  // Remove the existing rows
-  // Citation: https://stackoverflow.com/questions/7271490/delete-all-rows-in-an-html-table
-  d3.selectAll("tbody tr").remove();
   
-
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-
+  var inputElement = d3.select(".form-control");
+​
   // Get the value property of the input element
   var inputValue = inputElement.property("value");
-
-
+​
+  console.log(inputValue);
+  console.log(tableData);
+​
   var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-
+​
   console.log(filteredData);
-
-  // for each element within the data
-  filteredData.forEach(function(sightings) {
-    // console.log(sightings);
-    var tbody = d3.select("tbody");
-    // append a row to the table
-    var row = tbody.append("tr");
-    // for each item within the element
-    Object.entries(sightings).forEach(function([key, value]) {
-      // console.log(key, value);
-      // assign a variable to the new cells in the new row
-      var cell = row.append("td");
-      // input the value of the item into the cell
-      cell.text(value);
-
-      
-    });
-    
-  });
-}
-
+​}
